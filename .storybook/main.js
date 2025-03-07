d248ec93823b0ac323e3b9da4c8f234bd3430ead
Chris Brown <cabrown02@gmail.com>
2025-03-07 14:16:28 -0500

    Removed @storybook/experimental-nextjs-vite Added Webpack5 Added Babbel Added import statement for css

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,23 +1,42 @@
-
-
-/** @type { import('@storybook/nextjs').StorybookConfig } */
+/** @type { import('@storybook/react').StorybookConfig } */
 const config = {
-  "stories": [
+  stories: [
     "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
-  "addons": [
+
+  addons: [
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test"
+    "@storybook/experimental-addon-test",
+    "@storybook/addon-mdx-gfm",
+    "@storybook/addon-webpack5-compiler-babel"
   ],
-  "framework": {
-    "name": "@storybook/experimental-nextjs-vite",
-    "options": {}
+
+  framework: {
+    name: "@storybook/nextjs", // Use @storybook/react for a default React project
+    options: {},
   },
-  "staticDirs": [
-    "../public"
-  ]
+
+  staticDirs: ["../public"],
+
+  webpackFinal: (config) => {
+    // Add or modify Webpack configurations if needed
+    config.module.rules.push({
+      test: /\.js$/,
+      loader: "esbuild-loader",
+      options: {
+        loader: "jsx", // Handle JSX
+        target: "es2015",
+      },
+    });
+    return config;
+  },
+
+  docs: {
+    autodocs: true
+  }
 };
+
 export default config;
