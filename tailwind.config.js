a6eaa9210df096e14dfd1f0db2813b382e75039b
Chris Brown <cabrown02@gmail.com>
2025-03-12 14:53:09 -0400

    Added tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx}",
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./stories/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          // Add your custom colors here
+          // 'primary': '#ff0000',
+          // 'secondary': '#00ff00',
+        },
+        fontFamily: {
+          // Add your custom fonts here
+          // 'sans': ['Graphik', 'sans-serif'],
+          // 'mono': ['Space Mono', 'monospace'],
+            'nudista': ["Nudista", "sans-serif"],
+            'vcr-osd-mono': ["VCR OSD Mono", "sans-serif"],
+   
+        },
+        spacing: {
+          // Add custom spacing values
+          // '72': '18rem',
+          // '84': '21rem',
+          // '96': '24rem',
+        },
+        // Extend other theme properties as needed
+      },
+    },
+    plugins: [
+      // Add any Tailwind CSS plugins here
+      require('@tailwindcss/forms'),
+      // require('@tailwindcss/typography'),
+      // require('@tailwindcss/aspect-ratio'),
+    ],
+  }
