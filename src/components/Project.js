e948d82d7ce9b71801bb4c233660dc3b78b1ac05
Chris Brown <cabrown02@gmail.com>
2025-04-11 11:15:37 -0400

    Updated styles on project

--- src/components/Project.js
+++ src/components/Project.js
@@ -16,13 +16,13 @@ export default function Project({ title, subheading, technologies, description, 
 
     <div className="mb-12 border-b border-neutral-500 pb-6">
       {/* Project Title */}
-      <div className="flex gap-4 mb-4">
-      <h3 className="text-2xl/10 font-semibold">{title}</h3>
-      {projectLink && (
-        <Link href={projectLink} target="_blank" rel="noopener noreferrer">
-          <button className="bg-neutral-700 mx-auto block hover:bg-neutral-900 cursor-pointer text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out">view project</button>
-        </Link>
-      )}
+      <div className="sm:flex gap-4 mb-4">
+        <h3 className="text-2xl/10 font-semibold">{title}</h3>
+        {projectLink && (
+          <Link href={projectLink} target="_blank" rel="noopener noreferrer">
+            <button className="bg-neutral-700 block hover:bg-neutral-900 cursor-pointer text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out">view project</button>
+          </Link>
+        )}
       </div>
 
       {/* Subheading */}
@@ -30,7 +30,7 @@ export default function Project({ title, subheading, technologies, description, 
 
       {/* Project Description */}
       {description && (
-        <div className="text-base mb-8 mx-auto">
+        <div className="mb-8 mx-auto">
           <PortableText value={description} />
         </div>
       )}
